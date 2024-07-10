b3d17eaa1a2cf35ff92814bfa64000f526c5c4d3
bloodnighttw <emails@bntw.dev>
2024-07-10 20:17:25 +0800

    Add test code and macro

--- reginleif-macro/src/lib.rs
+++ reginleif-macro/src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use proc_macro::TokenStream;
+use syn::ItemStruct;
+
+fn impl_expire(ast:ItemStruct) -> TokenStream{
+    let ident = ast.ident;
+    let duration_filed = ast.fields.iter()
+        .find(
+            |x| -> bool {
+                let has_attr = x.attrs.iter().find(
+                    |i| i.path().segments.len() == 1 && i.path().segments[0].ident == "dur"
+                );
+                has_attr.is_some()
+            }
+        ).expect("you should have attr #[dur] in your std::time::Duration field");
+
+    let ident2 = duration_filed.ident.clone().unwrap();
+
+    let token = quote::quote! {
+        impl crate::utils::expiring_data::Expirable for #ident{
+            fn get_duration(&self) -> std::time::Duration {
+                self.#ident2
+            }
+        }
+    };
+
+    token.into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[proc_macro_derive(Expirable, attributes(dur))]
+pub fn time_sensitive(item:TokenStream) -> TokenStream{
+    let ast:ItemStruct = syn::parse(item).unwrap();
+    impl_expire(ast)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_derive(NoRefresh, attributes(dur))]
+pub fn refresh_panic(item:TokenStream) -> TokenStream{
+    let ast:ItemStruct = syn::parse(item).unwrap();
+    let ident = ast.ident;
+    let token = quote::quote! {
+        impl crate::utils::expiring_data::Refreshable for #ident{
+            async fn refresh(&mut self) -> anyhow::Result<()>{
+                panic!("The data struct can't use refresh function.");
+            }
+        }
+    };
+    
+    token.into()
 }
--- reginleif/src/utils/expiring_data.rs
+++ reginleif/src/utils/expiring_data.rs
@@ -5,7 +5,7 @@ use crate::utils::serde_convert::{local_to_string, string_to_local};
 use anyhow::Result;
 
 #[derive(Clone, Deserialize, Serialize, Debug, PartialEq)]
-pub struct ExpiringData<T> where T:Expirable{
+pub struct ExpiringData<T> where T:Expirable+Refreshable{
     pub data: T,
     #[serde(deserialize_with = "string_to_local", serialize_with = "local_to_string")]
     pub created_at: chrono::DateTime<Local>,
@@ -17,18 +17,19 @@ pub trait Expirable {
     /// Get the duration of the data.
     /// Use in the `is_expired` function.
     fn get_duration(&self) -> Duration;
-    
+
+}
+
+pub trait Refreshable{
     /// Refresh the data.
     /// if the data don't have want to refresh, just not impl it,
     /// and it will panic when call this function.
-    async fn refresh(&mut self) -> Result<()>{
-        panic!("The data struct can't use refresh function.");
-    }
+    async fn refresh(&mut self) -> Result<()>;
 }
 
 impl<T> ExpiringData<T>
 where
-    T: Expirable,
+    T: Expirable + Refreshable,
 {
     
     /// Check the data is expired or not.
@@ -61,5 +62,61 @@ where
         Ok(&self.data)
     }
 
+}
+
+impl<T> From<T> for ExpiringData<T> where T:Expirable + Refreshable{
+    fn from(data: T) -> Self{
+        Self{
+            data,
+            created_at: Local::now()
+        }
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use std::time::Duration;
+    use reginleif_macro::{Expirable, NoRefresh};
+    use crate::utils::expiring_data::{ExpiringData, Refreshable};
+
+    #[derive(Expirable,NoRefresh,Default)]
+    struct TestStruct1{
+        #[dur] duration: Duration
+    }
+
+    #[derive(Expirable,Default)]
+    struct TestStruct2{
+        #[dur] duration: Duration
+    }
+
+    impl Refreshable for TestStruct2{
+        async fn refresh(&mut self) -> anyhow::Result<()> {
+            Ok(()) // do nothing in test
+        }
+    }
+
+    #[tokio::test]
+    pub async fn test_expire(){
+        let test:ExpiringData<_> = TestStruct1::default().into();
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        assert!(test.is_expired())
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    pub async fn test_no_refresh(){
+        let mut test:ExpiringData<_> = TestStruct1::default().into();
+ 
+        test.refresh().await.expect("it's should be panic!");
+    }
+
+    #[tokio::test]
+    pub async fn test_expire2(){
+        let mut test:ExpiringData<_> = TestStruct2::default().into();
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        test.refresh().await.unwrap();
+        test.try_ref().await.unwrap();
+    }
+    
 }
 
