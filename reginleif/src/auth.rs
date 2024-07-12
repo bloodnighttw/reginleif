5d92a1f0cf133a7790c95d5cc8e2b9238f2dd49e
bloodnighttw <emails@bntw.dev>
2024-07-12 23:29:28 +0800

    Improve Docs

--- reginleif/src/auth.rs
+++ reginleif/src/auth.rs
@@ -1,6 +1,10 @@
+/// All the thing that related to Microsoft OAuth2.0 service. (using devicecode flow)
 pub mod microsoft;
+/// All the thing that related to Xbox service.
 pub mod xbox;
+/// All the thing that related to Minecraft service.
 pub mod minecraft;
+/// A wrapper for Account data.
 pub mod account;
 mod constant;
 
